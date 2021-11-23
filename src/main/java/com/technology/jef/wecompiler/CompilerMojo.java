f1a126a4041c60b7365319f4b0c43ed12d8d9e22
murmilad <murmilad@gmail.com>
2021-11-23 20:34:46 +0300

    new version

--- src/main/java/com/technology/jef/wecompiler/CompilerMojo.java
+++ src/main/java/com/technology/jef/wecompiler/CompilerMojo.java
@@ -8,15 +8,28 @@ import org.apache.maven.plugins.annotations.Parameter;
 import com.technology.jef.wecompiler.WECompiler;
 
 
-@Mojo( name = "jef-compiller")
+@Mojo( name = "generate-html")
 public class CompilerMojo  extends AbstractMojo {
 
-    @Parameter( property = "jef.interface-path", defaultValue = "src/main/resources/jef" )
+    @Parameter( property = "generate-html.interfacePath", defaultValue = "src/main/resources/jef" )
     private String interfacePath;
+    @Parameter( property = "generate-html.htmlPath", defaultValue = "src/main/webapp" )
+    private String htmlPath;
+    @Parameter( property = "generate-html.jsPath", defaultValue = "src/main/webapp/js" )
+    private String jsPath;
+    @Parameter( property = "generate-html.locale", defaultValue = "ru_RU" )
+    private String locale;
     
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." );
-        WECompiler.main(new String[]{interfacePath}); 
+        getLog().info( "generate-html.jefInterfacePath = " + interfacePath);
+        getLog().info( "generate-html.jefHTMLPath = " + htmlPath);
+        getLog().info( "generate-html.jefJsPath = " + jsPath);
+        getLog().info( "generate-html.locale = " + locale);
+        try  {
+            WECompiler.main(new String[]{interfacePath, htmlPath, jsPath, locale});
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        } 
     }
 }
